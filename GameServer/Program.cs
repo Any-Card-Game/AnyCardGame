4edff7f3c343107f4cc5753facec3aff03d3e839
Salvatore <dested@gmail.com>
2015-08-29 02:28:06 -0700

    added some basic logic for playing with jint

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -4,38 +4,149 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Jint;
 using Jint.Native;
+using Jint.Native.Function;
+using Jint.Native.Object;
+using Jint.Native.String;
+using Jint.Runtime;
+using Jint.Runtime.Descriptors;
+using Jint.Runtime.Interop;
 
 namespace GameServer
 {
+    public class Pile
+    {
+        public string Name { get; set; }
+        public Pile(string name)
+        {
+            Name = name;
+        }
+    }
+
+
+    public sealed class SalConstructor : FunctionInstance, IConstructor
+    {
+        private readonly Engine _engine;
+        public SalPrototype PrototypeObject { get; private set; }
+
+        private SalConstructor(Engine engine) : base(engine, null, null, false)
+        {
+            _engine = engine;
+        }
+
+        public static SalConstructor CreateObjectConstructor(Engine engine)
+        {
+            SalConstructor objectConstructor = new SalConstructor(engine);
+            objectConstructor.Extensible = true;
+            objectConstructor.PrototypeObject = SalPrototype.CreatePrototypeObject(engine, objectConstructor);
+            objectConstructor.FastAddProperty("length", (JsValue)1.0, false, false, false);
+            objectConstructor.FastAddProperty("prototype", (JsValue)((ObjectInstance)objectConstructor.PrototypeObject), false, false, false);
+            return objectConstructor;
+        }
+
+        public void Configure()
+        {
+            Prototype = Engine.Function.PrototypeObject;
+
+            Prototype.FastAddProperty("shoes", new ClrFunctionInstance(Engine, GetPrototypeOf, 1), true, false, true);
+        }
+
+        public JsValue GetPrototypeOf(JsValue thisObject, JsValue[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override JsValue Call(JsValue thisObject, JsValue[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObjectInstance Construct(JsValue[] arguments)
+        {
+            var name = arguments[0].AsString();
+            return (ObjectInstance) this.PrototypeObject;
+
+        }
+    }
+    public sealed class SalPrototype : ObjectInstance
+    {
+        private SalPrototype(Engine engine)
+          : base(engine)
+        {
+        }
+
+        public static SalPrototype CreatePrototypeObject(Engine engine, SalConstructor objectConstructor)
+        {
+            SalPrototype objectPrototype1 = new SalPrototype(engine);
+            objectPrototype1.Extensible = true;
+            SalPrototype objectPrototype2 = objectPrototype1;
+            objectPrototype2.FastAddProperty("constructor", (JsValue)((ObjectInstance)objectConstructor), true, false, true);
+            objectPrototype2.FastAddProperty("shoes", (JsValue)((ObjectInstance)new ClrFunctionInstance(engine, new Func<JsValue, JsValue[], JsValue>(getThing))), true, false, true);
+            return objectPrototype2;
+        }
+
+        private static JsValue getThing(JsValue arg1, JsValue[] arg2)
+        {
+            return arg2[0].AsNumber()*12;
+        }
+
+        public void Configure()
+        {
+        }
+
+    }
+
+
+
+
     class Program
     {
+        static Jint.Engine engine = new Jint.Engine();
         static void Main(string[] args)
         {
-
-            Jint.Engine engine = new Jint.Engine();
-            var typescript = File.ReadAllText("js/typescript/typescript.js");
-            Console.WriteLine("Read.");
-            var mc = engine.Execute(typescript);
-            Console.WriteLine("Parse.");
-            var tsc = mc.GetValue("ts");
-            var cc = tsc.AsObject().Get("transpile");
-            var j = @"class foo{bar(c){let j=15+c;return j+2;}}";
-            var c = cc.Invoke(j).AsString();
+            engine.SetValue("log", new Action<object>(a =>
+            {
+                
+            })); 
+            //todo make a generic way to add new classes and shit
+            engine.SetValue("Pile", SalConstructor.CreateObjectConstructor(engine));
+//            engine = engine.Execute("var j=new Pile('spades');log(j.doThing(15));");
+//            var cdc = engine.GetValue("j");
 
 
-            var mcc = engine.Execute(c);
-            mcc=mcc.Execute("var f=new foo()");
+
+            var sevens = File.ReadAllText("js/sevens.js");
+            var c = executeES6(sevens);
+
+
+
+            var mcc = c.Execute("var sevens=new Sevens()");
+            //do stuff to it
+            mcc = mcc.Execute("sevens.constructor();");
+
+
+            /*mcc = mcc.Execute("var f=new foo()");
             var fva = mcc.GetValue("f");
             var cd = fva.AsObject().Get("bar").Invoke(200);
-            var nn = cd.AsNumber();
-
-            Console.WriteLine("Trans.");
-            Console.WriteLine(nn);
+            var nn = cd.AsNumber();*/
 
 
             Console.WriteLine("Press any [Enter] to close the host.");
             Console.ReadLine();
+        }
+
+        private static Engine executeES6(string text)
+        {
+            var typescript = File.ReadAllText("js/typescript/typescript.js");
+            var mc = engine.Execute(typescript);
+            var tsc = mc.GetValue("ts");
+            var cc = tsc.AsObject().Get("transpile");
+
+            var c = cc.Invoke(text).AsString();
+
+            var mcc = engine.Execute(c);
+            return mcc;
         }
     }
 }
---
+++ GameServer/js/sevens.js
@@ -0,0 +1,235 @@
+﻿function Sevens() {
+    var self = this; 
+    self.constructor = function () {
+
+        self.spades = new Pile('spades');
+        self.clubs = new Pile('clubs');
+        self.hearts = new Pile('hearts');
+        self.diamonds = new Pile('diamonds');
+        log(self.spades.shoes(12));
+/*
+        self.cardGame.getSpaceByName('clubs').assignPile(self.clubs);
+        self.cardGame.getSpaceByName('spades').assignPile(self.spades);
+        self.cardGame.getSpaceByName('hearts').assignPile(self.hearts);
+        self.cardGame.getSpaceByName('diamonds').assignPile(self.diamonds);*/
+
+    }; 
+
+    self.runGame = function () {
+        if (!self.cardGame.users || self.cardGame.users.length == 0) {
+            console.log("baaad");
+            return true;
+        }
+        _.numbers(1, 20).foreach(function() {
+            self.cardGame.deck.cards = self.shuffle(self.cardGame.deck.cards);
+        });
+
+        self.cardGame.users.foreach(function(u, ind) {
+            //shuff.log('::' + u.userName);
+
+            self.cardGame.getSpaceByName('User' + ind).assignPile(u.cards).assignUser(u); 
+            self.cardGame.getTextByName('User' + ind).text = u.userName; 
+        });
+
+
+        while (self.cardGame.deck.cards.length > 0) {
+            self.cardGame.users.foreach(function(u) {
+                if (self.cardGame.deck.cards.length > 0) {
+                    self.cardGame.deck.cards[0].state = 2;
+                    u.cards.cards.push(self.cardGame.deck.cards[0]);
+                    self.cardGame.deck.cards.remove(self.cardGame.deck.cards[0]);
+                }
+            });
+        }
+
+
+        self.cardGame.users.foreach(function(u) {
+            u.cards.cards.sortCards();
+        });
+
+        var CardTypes = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
+        var CardNames = ['Ace', 'Deuce', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+
+        while (true) {
+            var result = self.cardGame.users.foreach(function(u) {
+
+                var usable = u.cards.cards.where(function(c) {
+                    return (c.type == 3 && (c.value == 6 || self.spades.cards.any(function(_c) {
+                        return _c.value == c.value + 1 || _c.value == c.value - 1;
+                    }))) ||
+                    (c.type == 1 && (c.value == 6 || self.clubs.cards.any(function(_c) {
+                        return _c.value == c.value + 1 || _c.value == c.value - 1;
+                    }))) ||
+                    (c.type == 2 && (c.value == 6 || self.hearts.cards.any(function(_c) {
+                        return _c.value == c.value + 1 || _c.value == c.value - 1;
+                    }))) ||
+                    (c.type == 0 && (c.value == 6 || self.diamonds.cards.any(function(_c) {
+                        return _c.value == c.value + 1 || _c.value == c.value - 1;
+                    })));
+                });
+                var answers = [];
+                answers.push('Skip');
+                usable.sortCards().foreach(function(card) {
+                    answers.push(CardNames[card.value] + ' Of ' + CardTypes[card.type]);
+                });
+
+              
+                var sp = self.cardGame.spaces;
+                for (var i = 0; i < sp.length; i++) {
+                    //sp[i].rotate += 10;
+
+                    sp[i].effects = [];
+
+
+                    if (sp[i].user == u) {
+                        if (usable.length == 0) {
+                            sp[i].effects.push("CurrentPlayerNoCards");
+                        } else {
+
+                            sp[i].effects.push("CurrentPlayer");
+                        }
+                    } else if (sp[i].user) {
+                        sp[i].effects.push("InactivePlayer");
+
+                    }
+
+
+                    if (!sp[i].user) {
+                        //25 + sp[i].pile.cards.length * 2
+                        sp[i].effects.push("CenterPiles");
+                    } else {
+                        sp[i].effects.push("Bend");
+                    }
+
+                    for (var ij = 0; ij < sp[i].pile.cards.length; ij++) {
+                        var card = sp[i].pile.cards[ij];
+                        card.effects = [];
+                        /*
+
+                        card.appearance.addEffect(new AnimatedEffect$Between(
+                            {
+                                from: { outer: { border: { all: "solid 2px black" }, padding: { all: "32px" }, } },
+                                to: { outer: { border: { all: "solid 10px black" }, padding: { all: "12px" }, } }
+                            }, 1000, "linear"
+                        )).chainEffect(new AnimatedEffect$Between(
+                            {
+                                from: { outer: { rotate: 45, } },
+                                to: { outer: { rotate: 97 } }
+                            }, 1000, "linear"
+                        )).chainEffect(new AnimatedEffect$Between(
+                            {
+                                from: { outer: { backColor: "#FFF", padding: { all: "52px" }, } },
+                                to: { outer: { backColor: "#DD1", padding: { all: "12px" }, } }
+                            }, 1000, "linear"
+                        )).chainEffect(new Effect$Highlight({
+                                radius: 19,
+                                color: 'rgba(1,1,1,0.22)',
+                                opacity: .55
+                            }
+                        )).chainEffect(new Effect$StyleProperty({ outer: { backColor: "#11F" } })).chainEffect(new AnimatedEffect$Between(
+                            {
+                                from: { outer: { rotate: 45, } },
+                                to: { outer: { rotate:97 } }
+                            }, 1000, "linear"
+                        ));
+
+                        card.addAction(new Action$CardDraggable({ droppableSpaces: [sp[i]], droppableLocations: [{ x: 0, y: 0, width: 1, height: 1 }] }));
+                            
+                            
+                            
+
+                        if(card.value==6 && !sp[i].user) {
+                        card.appearance.effects.push(new Effect$StyleProperty({outer:{border:{all:"solid 2px black"},padding:{all:"2px"},}}));
+                             
+                        }*/
+                        //shuff.log(sp[i].user);
+                        if (card.value == 6 && !sp[i].user) {
+
+                            card.effects.push("Seven");
+                        }
+
+                        for (var j = 0; j < usable.length; j++) {
+                            var m = usable[j];
+                            if (m.value == card.value && m.type == card.type) {
+                                card.effects.push("PlayableCard");
+                                break;
+                            }
+                        }
+
+
+                    }
+                }
+                //shuff.log('asking question');
+                var de = shuff.askQuestion(u, 'Which card would you like to play?', answers, self.cardGame);
+                //shuff.log('asked question: ' + de);
+
+                if (de > 0 && usable.length >= de) {
+                    var rm = usable[de - 1]; 
+                    rm.state = 0;
+
+                    switch (rm.type) {
+                    case 3:
+                        u.cards.cards.remove(rm);
+                        self.spades.cards.push(rm);
+                        self.spades.cards.sortCards().reverse();
+                        break;
+                    case 1:
+                        u.cards.cards.remove(rm);
+                        self.clubs.cards.push(rm);
+                        self.clubs.cards.sortCards().reverse();
+                        break;
+                    case 2:
+                        u.cards.cards.remove(rm);
+                        self.hearts.cards.push(rm);
+                        self.hearts.cards.sortCards().reverse();
+                        break;
+                    case 0:
+                        u.cards.cards.remove(rm);
+                        self.diamonds.cards.push(rm);
+                        self.diamonds.cards.sortCards().reverse();
+                        break;
+                    }
+
+                    if (u.cards.cards.length == 0) {
+
+                        for (var i = 0; i < sp.length; i++) {
+
+                            if (sp[i].user == u) {
+                                sp[i].effects.push("PlayerWon");
+                                break;
+                            }
+                        }
+                        shuff.declareWinner(u);
+
+                        return true;
+                    }
+                }
+                return false;
+            });
+            if (result) {
+                return true;
+            }
+
+        }
+
+
+    };
+
+
+    self.shuffle = function(arbs) {
+        var indes = 0;
+        var vafb = _.clone(arbs);
+
+        vafb.foreach(function(fs) {
+            var vm = _.floor(_.random() * vafb.length);
+            vafb[indes] = vafb[vm];
+            indes++;
+            vafb[vm] = fs;
+        });
+
+        arbs = vafb;
+
+        return arbs;
+    };
+    return self;
+};
