87540bae879839fbf5819aaa7e2b3260ef1fd22f
Salvatore <dested@gmail.com>
2015-08-29 00:31:09 -0700

    added support for mono running of the rest server

--- RestServer/Properties/AssemblyInfo.cs
+++ RestServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RestServer")]
+[assembly: AssemblyTitle("RestServer2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RestServer")]
+[assembly: AssemblyProduct("RestServer2")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f5a1ef9b-f09f-4091-8696-dd47e042528b")]
+[assembly: Guid("7b6305ee-08fb-45ee-aa8c-65f8699365d0")]
 
 // Version information for an assembly consists of the following four values:
 //
